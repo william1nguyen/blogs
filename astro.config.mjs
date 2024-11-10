fda6207869275bcb5a5ff4f91e252c340e499bd4
william1nguyen <williamnguyen.proxel@gmail.com>
2024-11-10 10:14:05 +0700

    feat: add ci flows

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://william1nguyen.github.io",
+  base: "/",
+  integrations: [mdx(), sitemap()],
 });
